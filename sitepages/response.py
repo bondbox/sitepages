3dc00a00efce5379c83336fd13fa3dd5045eb90a
Mingzhe Zou <zoumingzhe@qq.com>
2024-10-15 09:40:23 +0800

    response: add json format

--- sitepages/response.py
+++ sitepages/response.py
@@ -2,16 +2,18 @@
 
 from functools import wraps
 import traceback
+from typing import Any
 from typing import Callable
 
 from flask import Response
+from flask import jsonify
 from markdown import markdown
 
 
 class FlaskResponse:
 
     @classmethod
-    def html(cls, func: Callable[..., str]):
+    def html(cls, func: Callable[..., str]) -> Callable[..., Response]:
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
@@ -23,7 +25,19 @@ class FlaskResponse:
         return wrapper
 
     @classmethod
-    def plain(cls, func: Callable[..., str]):
+    def json(cls, func: Callable[..., Any]) -> Callable[..., Response]:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                content = func(*args, **kwargs)
+                return jsonify(content)
+            except Exception:
+                trace: str = traceback.format_exc()
+                return Response(response=trace, status=500, mimetype="text/plain")  # noqa:E501
+        return wrapper
+
+    @classmethod
+    def plain(cls, func: Callable[..., str]) -> Callable[..., Response]:
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
@@ -35,7 +49,7 @@ class FlaskResponse:
         return wrapper
 
     @classmethod
-    def markdown(cls, func: Callable[..., str]):
+    def markdown(cls, func: Callable[..., str]) -> Callable[..., Response]:
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
