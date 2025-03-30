5d9275e835c6a28970cacfbd66540dc2dabcd3fe
Mingzhe Zou <zoumingzhe@outlook.com>
2025-03-30 22:07:49 +0800

    bump version to 0.4

--- sitepages/attribute.py
+++ sitepages/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "sitepages"
-__version__ = "0.3"
+__version__ = "0.4"
 __urlhome__ = "https://github.com/bondbox/sitepages/"
 __description__ = "Web request module"
 
